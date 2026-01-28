eac25bfa50ec10f66f0ff9f91c3585b97ea0f6d7
Ramón Vila Ferreres <ramonvilafer@protonmail.com>
2026-01-28 22:30:06 +0100

    Initial Commit

---
+++ OpenWhyFi/OpenWhyFiApp.swift
@@ -0,0 +1,32 @@
+//
+//  OpenWhyFiApp.swift
+//  OpenWhyFi
+//
+//  Created by Ramon on 1/28/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct OpenWhyFiApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
